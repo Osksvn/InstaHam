caf13c3aae791d407c572a2ebba9a69870111936
osksvn <osksvn@outlook.com>
2020-06-30 17:51:52 +0200

    fixed placeholders for explore screen

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,85 +1,61 @@
-import { Ionicons } from "@expo/vector-icons";
-import * as WebBrowser from "expo-web-browser";
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { RectButton, ScrollView } from "react-native-gesture-handler";
-import { FlatList, Image } from "react-native";
+/*This is an Example of Grid View in React Native*/
+import React, { Component } from "react";
+//import rect in our project
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+//import all the components we will need
 
-export default function ExploreScreen() {
-  var images = getImages();
-  return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-    >
-      <FlatList
-        data={images}
-        renderItem={({ item }) => (
-          <View style={{ flex: 1, flexDirection: "column", margin: 1 }}>
-            <Image style={styles.imageThumbnail} source={{ uri: images }} />
-          </View>
-        )}
-        //Setting the number of column
-        numColumns={3}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </ScrollView>
-  );
-}
-
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton
-      style={[styles.option, isLastOption && styles.lastOption]}
-      onPress={onPress}
-    >
-      <View style={{ flexDirection: "row" }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
-}
-
-function getImages() {
-  var i;
-  var imageArray = [10];
-  for (i = 0; i <= 10; i++) {
-    imageArray[i] = "https://picsum.photos/250/250";
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dataSource: {},
+    };
   }
-
-  return imageArray;
+  componentDidMount() {
+    var that = this;
+    let items = Array.apply(null, Array(60)).map((v, i) => {
+      var random = Math.floor(Math.random() * 200) + 100;
+      return { id: i, src: "https://picsum.photos/" + random + "/" + random };
+    });
+    that.setState({
+      dataSource: items,
+    });
+  }
+  render() {
+    return (
+      <View style={styles.MainContainer}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => (
+            <View style={{ flex: 1, flexDirection: "column", margin: 1 }}>
+              <Image style={styles.imageThumbnail} source={{ uri: item.src }} />
+            </View>
+          )}
+          //Setting the number of column
+          numColumns={3}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  MainContainer: {
+    justifyContent: "center",
     flex: 1,
-    backgroundColor: "#fafafa",
+    paddingTop: 30,
   },
-  contentContainer: {
-    paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: "#fdfdfd",
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: "#ededed",
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: "flex-start",
-    marginTop: 1,
+  imageThumbnail: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 100,
   },
 });
